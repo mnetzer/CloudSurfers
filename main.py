4a5da15eff9d166213a0b1978b0475e6d6ae5cd1
mnetzer <max.netzer@web.de>
2023-11-16 15:29:36 +0100

    Routes

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ def pickup():
 def cancel():
     return render_template('dashboard.html')
 
+@app.route('/pickup_unloggedin')
+def pickup_unloggedin():
+    return render_template('pickup_unloggedin.html')
+
+@app.route('/login3')
+def login3():
+    return render_template('login3.html')
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
 
