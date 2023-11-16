cd12e0010bb89b20643a99ed928f5aff1a5cace4
mnetzer <max.netzer@web.de>
2023-11-16 15:19:34 +0100

    Clean up

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from app import create_app, db
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('dashboard.html')
+
+@app.route('/pickup')
+def pickup():
+    return render_template('pickup.html')
+
+@app.route('/cancel')
+def cancel():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
-
 
